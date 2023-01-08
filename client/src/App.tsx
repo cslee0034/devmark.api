1d67851a99c9c27f933baa0d634cb6cd8aeda90e
cslee <cslee0034@gmail.com>
2023-01-09 01:20:42 +0900

    feat: Add Toggle and Add fix design
    useContext를 활용한 toggle버튼 구현
    UserName 드롭다운 수정
    search 디자인 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
 import axios from "axios";
-import { useEffect } from "react";
+import React, { createContext, useEffect, useMemo, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Slidebar from "./components/Slidebar";
 import Navbar from "./components/NavBar";
-import FrontPage from "./components/views/FrontPage";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import React from "react";
-import "./App.css";
 import Footer from "./components/Footer";
 
+import About from "./components/views/About";
+import FrontPage from "./components/views/FrontPage";
+import BookmarkPage from "./components/views/BookmarkPage";
+import LoginPage from "./components/views/LoginPage";
+import RegisterPage from "./components/views/RegisterPage";
+
+import "./App.css";
+
+export const UserContext = createContext({
+  setLoggedIn: (loggedIn: any): any => {},
+  setSidebar: (sidebar: any): any => {},
+});
+
 const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [sidebar, setSidebar] = useState(true);
+
+  const value = useMemo(
+    () => ({ setLoggedIn, setSidebar }),
+    [setLoggedIn, setSidebar]
+  );
+
   const callApi = async () => {
     axios.get("/api").then((res) => {
       console.log(res.data.test);
@@ -20,44 +39,56 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div id="page-top">
-      {/* Page Wrapper */}
-      <div id="wrapper">
-        {/* Sidebar */}
-        <div id="sidebar">
-          <Slidebar />
-        </div>
-        {/* End of Sidebar */}
-
-        {/* Content Wrapper */}
-        <div id="content-wrapper">
-          {/* Main Content */}
-          <div id="content">
-            {/* Navbar Content */}
-            <div id="navbar">
-              <Navbar />
+    <UserContext.Provider value={value}>
+      <div id="page-top">
+        {/* Page Wrapper */}
+        <div id="wrapper">
+          {/* Sidebar */}
+          {sidebar ? (
+            <div id="sidebar">
+              <Slidebar />
             </div>
-            {/* End of Navber Content */}
+          ) : null}
+          {/* End of Sidebar */}
 
-            {/* Begin Page Content */}
-            <div id="main-page">
-              <FrontPage />
+          {/* Content Wrapper */}
+          <div id="content-wrapper">
+            {/* Main Content */}
+            <div id="content">
+              {/* Navbar Content */}
+              <div id="navbar">
+                <Navbar />
+              </div>
+              {/* End of Navber Content */}
+
+              {/* Begin Page Content */}
+              <div id="main-page">
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<FrontPage />} />
+                    <Route path="/bookmark" element={<BookmarkPage />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                  </Routes>
+                </BrowserRouter>
+              </div>
+              {/* End of Page Content */}
             </div>
-            {/* End of Page Content */}
+
+            {/* End of Main Content */}
           </div>
-
-          {/* End of Main Content */}
+          {/* End of Content Wrapper */}
         </div>
-        {/* End of Content Wrapper */}
-      </div>
-      {/* Footer */}
-      <div>
-        <Footer />
-      </div>
-      {/* End of Footer */}
+        {/* Footer */}
+        <div>
+          <Footer />
+        </div>
+        {/* End of Footer */}
 
-      {/* End of Page Wrapper */}
-    </div>
+        {/* End of Page Wrapper */}
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { UserContext } from "../App";
 
 const NavBar: React.FC = () => {
-  const [toggle, setToggle] = useState(false);
+  const { setSidebar } = useContext(UserContext);
 
   return (
     // Navbar
     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
       {/* Slidebar Toggle */}
       <button
-        id="sidebarToggleTop"
-        className="btn btn-link d-md-none rounded-circle mr-3"
+        type="button"
+        className="btn sidebar-toggle"
         // onclickEvent Toggle
-        onClick={() => {
-          setToggle((e) => !e);
-        }}
+        onClick={() => setSidebar((prev: any) => !prev)}
       >
         <FontAwesomeIcon icon={faBars} />
       </button>
 
       {/* Search */}
-      <nav className="d-none d-sm-inline-block form-inline navbar-search">
+      <nav className="d-sm-inline-block form-inline navbar-search">
         <div className="container-fluid">
           <form className="d-flex">
             <input
@@ -33,7 +33,7 @@ const NavBar: React.FC = () => {
               aria-label="Search"
             />
             <button className="btn btn-outline-* btn-search" type="submit">
-              Search
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </form>
         </div>
@@ -52,18 +52,20 @@ const NavBar: React.FC = () => {
         <div className="vr m-2"></div>
 
         {/* Dropdown UserName */}
-        <div className="nav-item dropdown-center username ">
-          <div
-            className="data-toggle nav-link active username-item"
+        <div className="btn-group username">
+          <button
+            type="button"
+            className="btn data-toggle username-item"
             data-bs-toggle="dropdown"
+            data-bs-display="static"
             aria-expanded="false"
           >
             <span className="namespace">UserName&nbsp;</span>
             <div>
               <FontAwesomeIcon icon={faUser} />
             </div>
-          </div>
-          <ul className="dropdown-menu">
+          </button>
+          <ul className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
             <li>
               <a className="dropdown-item" href="#">
                 Profile
