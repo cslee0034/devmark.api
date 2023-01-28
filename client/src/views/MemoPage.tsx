1062608ad0c47a6c57a50ae9bc9bb43827e5ae24
cslee <cslee0034@gmail.com>
2023-01-28 15:34:16 +0900

    feat: Add Datapicker
    리액트 달력 기능을 추가했습니다

--- client/src/views/MemoPage.tsx
+++ client/src/views/MemoPage.tsx
@@ -1,43 +1,12 @@
 import React from "react";
+import Edit from "../utils/Edit";
 
 const MemoPage = (): JSX.Element => {
+  const temp = "";
   return (
-    <div className="mainpage-container">
-      {/* Header */}
-      <h3 className="main-header">Memo</h3>
-
-      {/* Content */}
-      <div className="card memo-card">
-        <div className="card-header">2023-01</div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">05</li>
-          <li className="list-group-item">04</li>
-          <li className="list-group-item">03</li>
-          <li className="list-group-item">02</li>
-          <li className="list-group-item">01</li>
-        </ul>
-      </div>
-
-      {/* Button */}
-
-      <div
-        className="btn-toolbar mb-3"
-        role="toolbar"
-        aria-label="Toolbar with button groups"
-      >
-        <div className="btn-group me-2" role="group" aria-label="First group">
-          <button type="button" className="btn btn-outline-secondary">
-            {"<"}
-          </button>
-          <button type="button" className="btn btn-outline-secondary disabled btn-center-disable">
-            &nbsp;&nbsp;
-          </button>
-          <button type="button" className="btn btn-outline-secondary">
-            {">"}
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <Edit search={temp}/>
+    </>
   );
 };
 
