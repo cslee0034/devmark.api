72caa3bf2cb4c6fc7ec201d0030e8f658eca1448
cslee <cslee0034@gmail.com>
2023-02-13 19:44:59 +0900

    feat: Start nest server
    nest 프레임워크로 기존 서버를 포팅합니다.

--- server/routes/info.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-export const info = express.Router();
-
-info.get("/", (req, res) => {
-  const userInfo = res.locals!.user;
-  userInfo.password = "";
-  res.send(userInfo);
-});
