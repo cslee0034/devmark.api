d6762000b5b71cb160929c7b10ec744878c0581d
cslee <cslee0034@gmail.com>
2023-02-14 04:01:58 +0900

    fix: Add Max length to DTO nick

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
@@ -7,6 +7,7 @@ export class CreateUserDto {
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(15)
   nick: string;
 
   @IsString()
