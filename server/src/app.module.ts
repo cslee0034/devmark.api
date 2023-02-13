e9cb1ca5b0d416916be22556c0b47ddf957a681d
cslee <cslee0034@gmail.com>
2023-02-13 23:50:34 +0900

    chore: import config, typeorm module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,3 +1,5 @@
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
