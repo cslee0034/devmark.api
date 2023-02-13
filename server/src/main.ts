60ef240bff3dc691000fb1bda3dfd4ee936f10da
cslee <cslee0034@gmail.com>
2023-02-14 00:44:43 +0900

    feat: Add global pipe
    글로벌 파이프를 설정 했습니다.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
 bootstrap();
