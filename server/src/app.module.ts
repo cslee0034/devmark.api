3c408e7009f4f684dca9a84bda4acf4f6cf40749
cslee <cslee0034@gmail.com>
2023-02-15 00:20:35 +0900

    fix: TypeOrm config setting
    typeorm의 config을 수정했습니다.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,21 +5,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserEntity } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      // envFilePath: process.env.NODE_ENV === 'dev' ? 'dev' : 'test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      // pord 환경일 때는 configModuel이 환경변수 파일을 무시
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: 3306,
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [UserEntity],
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      // entities: ['src/**/*.entity{.ts,.js}'] load
       synchronize: true,
+      // 변경사항 업데이트
+      dropSchema: true,
+      // row 삭제
+      logging: true,
+      entities: ['src/**/*.entity{.ts,.js}'],
     }),
     UserModule,
   ],
