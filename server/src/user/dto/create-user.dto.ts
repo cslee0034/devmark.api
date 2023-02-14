b6b4e837320578e913b42ee39fbbfd7b9e92d33e
cslee <cslee0034@gmail.com>
2023-02-15 03:24:12 +0900

    feat: Add dto message

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -2,15 +2,15 @@ import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '이메일은 비워져 있을 수 없습니다.' })
   email: string;
 
   @IsString()
-  @IsNotEmpty()
-  @MaxLength(15)
+  @IsNotEmpty({ message: '닉네임은 비워져 있을 수 없습니다.' })
+  @MaxLength(15, { message: '닉네임은 최대 15글자입니다.' })
   nick: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '비밀번호는 비워져 있을 수 없습니다.' })
   password: string;
 }
