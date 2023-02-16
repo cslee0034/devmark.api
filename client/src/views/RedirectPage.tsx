eb684dd8d3a8061c5ccdbf6b068e8b0df46693cd
cslee <cslee0034@gmail.com>
2023-02-17 02:48:02 +0900

    feat: fix redirection code

--- client/src/views/RedirectPage.tsx
+++ client/src/views/RedirectPage.tsx
@@ -1,23 +1,38 @@
 import axios, { AxiosResponse } from "axios";
 import React, { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 interface Get {
   Error: any;
-  id: number;
-  nick: string;
+  user: {
+    id: number;
+    nick: string;
+    provider: string;
+  };
 }
 
 const RedirectPage = (): JSX.Element => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const access_token = searchParams.get("access_token");
+
   const getInfo = async () => {
     try {
-      await axios.get<Get>("/api/info").then((res) => {
-        const UserId = String(res.data.id);
-        const UserNick = String(res.data.nick);
-        window.sessionStorage.setItem("userId", UserId);
-        window.sessionStorage.setItem("userNick", UserNick);
+      await axios
+        .get<Get>("/api/user/info", {
+          headers: { Authorization: `Bearer ${access_token}` },
+        })
+        .then((res) => {
+          const UserId = String(res.data.user.id);
+          const UserNick = String(res.data.user.nick);
+          const Provider = String(res.data.user.provider);
+          window.localStorage.setItem("userId", UserId);
+          window.localStorage.setItem("userNick", UserNick);
+          window.localStorage.setItem("provider", Provider);
+          window.localStorage.setItem("token", access_token!);
+          // 로컬에서 변조한 토큰임
 
-        window.location.replace("/");
-      });
+          window.location.replace("/");
+        });
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error(
