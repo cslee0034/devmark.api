fdb3a23757f4415a2e732c8ee2b1386598909b7e
cslee <cslee0034@gmail.com>
2023-02-24 07:54:31 +0900

    chore: Add type alarm

--- server/src/alarm/alarm.controller.ts
+++ server/src/alarm/alarm.controller.ts
@@ -1,16 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Post, Delete, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 import { ReqWithUserId } from 'src/common/decorators/req_user_id.decorator';
 import { AlarmService } from './alarm.service';
-import { CreateAlarmDto } from './dto/create-alarm.dto';
 import { AlarmEntity } from './entities/alarm.entity';
 
 @Controller('api/alarm')
@@ -31,13 +22,15 @@ export class AlarmController {
 
   @UseGuards(JwtAuthGuard)
   @Get('notification')
-  findAll_notification_alarm(@ReqWithUserId() body) {
+  findAll_notification_alarm(@ReqWithUserId() body): Promise<AlarmEntity[]> {
     return this.alarmService.findNotification(body.user_id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete('')
-  remove_alarm(@ReqWithUserId() body) {
+  remove_alarm(
+    @ReqWithUserId() body,
+  ): Promise<{ status: number; success: boolean }> {
     return this.alarmService.remove(body);
   }
 }
