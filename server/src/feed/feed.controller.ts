1e5ef6b7d009533ebf61eab5268e9f7232604a23
cslee <cslee0034@gmail.com>
2023-02-24 09:38:18 +0900

    fix: router fix

--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -11,32 +11,22 @@ import { FeedService } from './feed.service';
 import { CreateFeedDto } from './dto/create-feed.dto';
 import { UpdateFeedDto } from './dto/update-feed.dto';
 
-@Controller('feed')
+@Controller('api/feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
-  @Post()
-  create(@Body() createFeedDto: CreateFeedDto) {
+  @Post('')
+  create_feed(@Body() createFeedDto: CreateFeedDto) {
     return this.feedService.create(createFeedDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('')
+  find_feed() {
     return this.feedService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.feedService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFeedDto: UpdateFeedDto) {
-    return this.feedService.update(+id, updateFeedDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove_feed(@Param('id') id: string) {
     return this.feedService.remove(+id);
   }
 }
