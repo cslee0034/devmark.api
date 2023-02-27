132719ad510cf1a45b782951f494dede65c3fe9c
cslee <cslee0034@gmail.com>
2023-02-27 16:05:24 +0900

    feat: Add Exception filter

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,44 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import { utilities, WinstonModule } from 'nest-winston';
-import * as winstonDaily from 'winston-daily-rotate-file';
-import * as winston from 'winston';
-import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
-
-const logDir = __dirname + '../../logs';
-
-const dailyOptions = (level: string) => {
-  return {
-    level,
-    datePattern: 'YYYY-MM-DD',
-    dirname: logDir + `/${level}`,
-    filename: `%DATE%.${level}.log`,
-    maxFiles: 30, //30일치 로그파일 저장
-    zippedArchive: true, // 로그가 쌓이면 압축하여 관리
-  };
-};
+import { AllExceptionFiller } from './common/exception/exception.fillter';
+import { winstonLogger } from './common/utils/logger.winston';
+import * as path from 'path';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        new winston.transports.Console({
-          level: process.env.NODE_ENV === 'prod' ? 'info' : 'silly',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            utilities.format.nestLike('Dev-mark', {
-              prettyPrint: true,
-            }),
-          ),
-        }),
-        new winstonDaily(dailyOptions('info')),
-        new winstonDaily(dailyOptions('warn')),
-        new winstonDaily(dailyOptions('error')),
-      ],
-    }),
+    logger: winstonLogger,
   });
   app.useStaticAssets(path.join(__dirname, '../', 'uploads'), {
     // const app = await NestFactory.create<NestExpressApplication>
@@ -47,6 +18,7 @@ async function bootstrap() {
     prefix: '/img',
     // static file 제공 주소.
   });
+  app.useGlobalFilters(new AllExceptionFiller(winstonLogger));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
