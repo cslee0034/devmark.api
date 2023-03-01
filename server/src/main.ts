0ada5eea4877f4c406c7a5113d5a58d1ad0717ea
cslee <cslee0034@gmail.com>
2023-03-01 21:07:40 +0900

    Revert "feat: Add cors option default: allow all"
    This reverts commit 9db1780d5bac25dc6ce23fe9c2c9ba71ce73fe3a.

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,10 +18,6 @@ async function bootstrap() {
     prefix: '/img',
     // static file 제공 주소.
   });
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
   app.useGlobalFilters(new AllExceptionFiller(winstonLogger));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
