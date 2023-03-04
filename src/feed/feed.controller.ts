1349600d983eb42c820f8c6d7eb753ba07462ef3
cslee <cslee0034@gmail.com>
2023-03-04 23:14:18 +0900

    fix: delete authguard feed, add access modifier
    피드에 authguard를 제거해 로그인 하지 않은 유저도 접근할 수
    있도록 수정 했습니다.
    피드 삭제에 유저 access modifier를 추가 했습니다.

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -13,6 +13,8 @@ import { ReqWithUserId } from 'src/common/decorators/req_user_id.decorator';
 import { FeedEntity } from './entities/feed.entity';
 import { CreateFeedDto } from './dto/create-feed.dto';
 import { DeleteFeedDto } from './dto/delete-feed.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
 
 @Controller('api/feed')
 export class FeedController {
@@ -26,7 +28,6 @@ export class FeedController {
     return this.feedService.create(body);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('')
   find_feed(
     @Query('id', ParseIntPipe) id: number,
@@ -39,8 +40,9 @@ export class FeedController {
   @UseGuards(JwtAuthGuard)
   @Delete('')
   remove_feed(
+    @CurrentUser() user: UserEntity,
     @ReqWithUserId() body: DeleteFeedDto,
   ): Promise<{ status: number; success: boolean }> {
-    return this.feedService.remove(+body.id);
+    return this.feedService.remove(user, +body.id);
   }
 }
